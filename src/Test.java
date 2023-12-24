15f38414fa04e7dd48b2b3f80f8f5e793aed874c
zezik1987 <s.m.deriapa@student.khai.edu>
2023-12-24 12:01:17 +0200

    add one more comment Test.java  to feature_branch

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("education");
+        System.out.println("DICT");
         //comment on second string of the func
+        System.out.println("education");
     }
 }
