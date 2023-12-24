5408aa0ae59e2a4b943628f7f03efcdf19ed3a89
zezik1987 <s.m.deriapa@student.khai.edu>
2023-12-24 12:24:11 +0200

    ChatBot2/0

--- src/Chatbot/Chatbot.java
+++ src/Chatbot/Chatbot.java
@@ -4,7 +4,7 @@ public class Chatbot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! My name is Stell.");
+        System.out.println("Hello! My name is unititled.");
         System.out.println("I was created in 2023.");
 
         System.out.println("Please, remind me your name.");
